ba5efdc42907beeacff99c1f4d2c8983866fadb9
Kaleb Drew Smith <kalebjamsqk9@gmail.com>
2025-12-08 00:41:05 -0600

    Initial Commit

---
+++ Hello World/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Hello World
+//
+//  Created by Kaleb Drew Smith on 12/8/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
